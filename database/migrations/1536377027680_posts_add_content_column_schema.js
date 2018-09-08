1e78446f3e4f2b99db275b3abdebab61f03d85e9
liwenliang <liwenliang@yixia.com>
2018-09-08 11:35:50 +0800

    创建Migration修改已有的数据表

---
+++ database/migrations/1536377027680_posts_add_content_column_schema.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class PostsAddContentColumnSchema extends Schema {
+  up () {
+    this.table('posts', (table) => {
+      table.text('content', 'longtext')
+    })
+  }
+
+  down () {
+    this.table('posts', (table) => {
+      // reverse alternations
+      table.dropColumn('content')
+    })
+  }
+}
+
+module.exports = PostsAddContentColumnSchema
